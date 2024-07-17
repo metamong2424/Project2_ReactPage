bc4dc2aca0ade3befd70491d32be07559542df09
metamong2424 <tripleluck24@naver.com>
2024-07-17 17:09:29 +0900

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>빙빙 돌아가는 회전목마처럼</p>
+        <a
+          className="App-link"
+          href="https://www.hanwhaeagles.co.kr/index.do"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          한화이글스 홈페이지
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
